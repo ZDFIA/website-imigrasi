bed292565c2cbe070e6a7be84b8ff2b26c3512b3
divamaisyaraasofyan-wq <divamaisyaraasofyan@gmail.com>
2026-02-09 12:23:55 +0700

    main

--- hamburger.js
+++ hamburger.js
@@ -1,3 +1,5 @@
 function toggleMenu() {
   document.querySelector('.nav.nav-button').classList.toggle('show');
+  document.querySelector('.hamburger').classList.toggle('active');
+  document.querySelector('.header').classList.toggle('menu-open');
 }
